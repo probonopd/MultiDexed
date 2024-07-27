c40e939984b4436c0ec9dad25f858ec3892dfdec
probonopd <probonopd@users.noreply.github.com>
2024-07-27 11:43:38 +0000

    Still crashed before

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -67,20 +67,22 @@ PluginAudioProcessorEditor::PluginAudioProcessorEditor(PluginAudioProcessor &p)
 
 PluginAudioProcessorEditor::~PluginAudioProcessorEditor() {
 
-    // Remove all tabs from the tabbed component
+// Remove all tabs from the tabbed component
     if (tabbedComponent) {
         tabbedComponent->clearTabs();
+        tabbedComponent = nullptr;
     }
 
     // Clean up Dexed components and detach slider attachments
     for (int i = 0; i < audioProcessor.numberOfInstances; i++) {
-        if (dexedComponents[i]) {
+        if (i < dexedComponents.size() && dexedComponents[i]) {
             dexedComponents[i] = nullptr;
         }
-        dexedEditors[i] = nullptr;
+        if (i < dexedEditors.size()) {
+            dexedEditors[i] = nullptr;
+        }
     }
 
-    tabbedComponent = nullptr;
     detuneSliderAttachment = nullptr;
     panSliderAttachment = nullptr;
 }
